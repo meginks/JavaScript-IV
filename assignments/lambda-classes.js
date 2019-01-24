66be40e799d2cc895d953d618df99f4d5b5e3c99
unknown <38929577+meginks@users.noreply.github.com>
2019-01-24 15:59:52 -0600

    finished object instantiations and tests

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,5 +1,7 @@
 // CODE here for your Lambda Classes
 
+//CLASSES 
+
 class Person {
     constructor(attributes) {
         this.name = attributes.name;
@@ -8,7 +10,7 @@ class Person {
         this.gender = attributes.gender;
     }
     speak() {
-        return `Hello, my name is ${this.name}, and I am from ${this.location}`;
+        return `Hello, my name is ${this.name}, and I am from ${this.location}.`;
     }
 };
 
@@ -35,13 +37,13 @@ class Student extends Person {
         this.favSubjects = studentAttrs.favSubjects; // make array in instances
     }     
     listsSubjects() {
-        return ${this.favSubjects}.toString(" ");
+        return `${this.favSubjects}`.toString(" ");
     }
     PRAssignment(subject) {
-        return `${student.name} has submitted a PR for {subject}`;
+        return `${this.name} has submitted a PR for ${subject}.`;
     }
     sprintChallenge(subject) {
-        return `${student.name} has begun sprint challenge on ${subject}`;
+        return `${this.name} has begun sprint challenge on ${subject}.`;
     }
 
 }
@@ -56,7 +58,125 @@ class ProjectManager extends Instructor {
     return `${this.name} announces to ${channel}, @channel standy times!`;
     }
     debugsCode(student, subject) {
-        return `${this.name} debugs ${student.name}'s code in ${subject}`;
+        return `${this.name} debugs ${student.name}'s code in ${subject}.`;
     
     }
-}
+};
+
+// OBJECTS 
+
+/** Person Objects **/
+
+const joe = new Person({
+    name: "Joe",
+    age: 30, 
+    location: "Denver",
+    gender: "male"
+});
+ 
+const sarah = new Person({
+    name: "Sarah",
+    age: 35, 
+    location: "Ontario",
+    gender: "female"
+});
+
+
+/** Instructor Objects **/
+
+const mcgonagall = new Instructor({
+    name: "Professor McGonagall",
+    age: "old", 
+    location: "Hogwarts",
+    gender: "female",
+    specialty: "Transfiguration", 
+    favLanguage: "English",
+    catchPhrase: "Why is it always you three?"
+})
+
+const snape = new Instructor({
+    name: "Professor Snape", 
+    age: "mid 40s", 
+    location: "Hogwarts",
+    specialty: "Potions",
+    favLanguage: "English",
+    catchPhrase: "Potter!!!!!!!!"
+})
+
+
+/** Student Objects **/ 
+
+const harrypotter = new Student({
+    name: "Harry Potter", 
+    age: 17,
+    location: "Hogwarts",
+    gender: "male",
+    previousBackground: "lived in the cupboard under the stairs",
+    className: "Griffindor",
+    favSubjects: ["Defense Against the Dark Arts", "Transfiguration", "Quiddich"],
+}); 
+
+const ron = new Student({
+    name: "Ron Weasley",
+    age: 17, 
+    location: "Hogwarts",
+    gender: "male", 
+    previousBackground: "he's a Weasley",
+    className: "Griffindor",
+    favSubjects: ["lunch", "quiddich"]
+}); 
+
+const hermione = new Student({
+    name: "Hermione Granger",
+    age: 17,
+    location: "Hogwarts",
+    gender: "female",
+    previousBackground: "Parents are muggle dentists",
+    favSubjects: ["Transfiguration", "Potions", "Divination", "Defense Against the Dark Arts", "Literally everything"]
+})
+
+/** Project Manager Objects **/
+
+const percy = new ProjectManager({
+    name: "Percy Weasley",
+    age: "older than Ron",
+    location: "Hogwarts",
+    gender: "male",
+    specialty: "Being a prefect",
+    favLanguage: "English",
+    gradClassName: "Griffindor",
+    favInstructor: mcgonagall
+})
+
+const lucius = new ProjectManager({
+    name: "Lucius Malfoy",
+    age: "super old",
+    location: "Hogwarts",
+    gender: "male",
+    specialty: "Being mean and having nice hair",
+    favLanguage: "Snark",
+    gradClassName: "Slytherin",
+    favInstructor: "uhhh"
+})
+
+// TESTS  
+
+console.log(joe.speak());
+console.log(sarah.speak());
+console.log(mcgonagall.speak());
+console.log(mcgonagall.catchPhrase);
+console.log(snape.catchPhrase);
+console.log(snape.speak());
+console.log(snape.demo("Potions")); 
+console.log(mcgonagall.grade(hermione, "Transfiguration"));
+console.log(harrypotter.speak());
+console.log(ron.speak());
+console.log(hermione.speak());
+console.log(hermione.listsSubjects());
+console.log(ron.PRAssignment("Potions"));
+console.log(harrypotter.sprintChallenge("Divination"));
+console.log(percy.speak());
+console.log(percy.standUp("griffindor_firstyears")); 
+console.log(lucius.standUp("slytherin_firstyears"));
+console.log(percy.debugsCode(ron, "Potions"));
+
