2e2878f082bd6b71a2b5577d8c0a48faf1339552
unknown <38929577+meginks@users.noreply.github.com>
2019-01-24 14:30:02 -0600

    copy-pasted over js iii code

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,206 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+/*
+  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+
+  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+  
+  Each constructor function has unique properties and methods that are defined in their block comments below:
+*/
+console.log("this is where prototypes.js starts");
+/*
+  === GameObject ===
+  * createdAt
+  * dimensions (These represent the character's size in the video game)
+  * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
+*/
+
+function GameObject (properties) {
+this.createdAt = properties.createdAt;
+this.dimensions = properties.dimensions;
+  this.destroy = function () {
+    console.log(`${this.name} was removed from the game`);
+    }
+};
+
+/*
+GameObject.prototype.destroy = function() {
+  console.log('${this.name} was removed from the game.');
+}
+
+/*
+  === CharacterStats ===
+  * healthPoints
+  * name
+  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * should inherit destroy() from GameObject's prototype
+*/ 
+
+function CharacterStats (attributes) {
+  this.healthPoints = attributes.healthPoints;
+  this.name = attributes.name;
+};
+
+CharacterStats.prototype.takeDamage = function() {
+  console.log(`${this.name} took damage`);
+}
+
+/*
+  === Humanoid (Having an appearance or character resembling that of a human.) ===
+  * team
+  * weapons
+  * language
+  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  * should inherit destroy() from GameObject through CharacterStats
+  * should inherit takeDamage() from CharacterStats
+*/
+ 
+function Humanoid (attributes) {
+  GameObject.call(this, attributes);
+  CharacterStats.call(this, attributes);
+  this.team = attributes.team;
+  this.weapons = attributes.weapons;
+  this.language = attributes.language;
+};
+
+Humanoid.prototype = Object.create(GameObject.prototype);
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+
+Humanoid.prototype.greet = function () {
+  console.log(`${this.name} offers a greeting in ${this.language}`);
+}
+
+
+/*
+  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+  * Instances of CharacterStats should have all of the same properties as GameObject.
+*/
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
+
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
+
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table 
+  console.log(mage.weapons); // Staff of Shamalama 
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+  // Stretch task: 
+  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+  // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+
+  function Villain (attributes) {
+    this.evilName = attributes.evilName;
+    this.nickName = attributes.nickName;
+    Humanoid.call(this, attributes);
+    this.avadaKedavra = function (victim) {
+      if (victim.healthPoints > 0) {
+        victim.healthPoints -= 1; 
+        console.log(`${victim.name} now has ${victim.healthPoints} health points`)
+      } if (victim.healthPoints === 0) {
+        victim.destroy();
+      } else {
+        console.log(`The spell didn't work! ${victim.name} is still alive, but wounded!`);
+      }
+    }
+  };
+
+  const voldemort = new Villain({
+    name: 'Tom Riddle',
+    evilName: 'Voldemort',
+    nickName: 'He who must not be named',
+    healthPoints: 100,
+    language: ['Parseltongue', 'English'],
+    weapons: ['wand'] 
+  });
+
+  console.log(voldemort);
+
+  function Hero (attributes) {
+    Humanoid.call(this, attributes);
+    this.goodName = attributes.goodName;
+    this.nickName = attributes.nickName;
+    this.expelliarmus = function (subject) {
+        console.log(`${subject.evilName} drops his ${subject.weapons}!`);
+    };
+  } 
+
+  const harry = new Hero({
+    name: 'Harry Potter', 
+    goodName: 'Harry', 
+    nickName: 'the Chosen One', 
+    healthPoints: 100, 
+    team: 'Griffindor',
+    language: ['Parseltongue', 'English'],
+    weapons: ['wand'], 
+  })
+
+  console.log(harry); 
+
+
+  voldemort.avadaKedavra(harry);
+
+  harry.expelliarmus(voldemort);
