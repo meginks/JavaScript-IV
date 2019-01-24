edd04f356608fc44a489274408c665cd538fcae4
unknown <38929577+meginks@users.noreply.github.com>
2019-01-24 15:09:07 -0600

    finished writing classes for lambda school

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -14,6 +14,7 @@ class Person {
 
 class Instructor extends Person {
     constructor(instructorAttrs) {
+        super(instructorAttrs);
         this.specialty = instructorAttrs.specialty;
         this.favLanguage = instructorAttrs.favLanguage;
         this.catchPhrase = instructorAttrs.catchPhrase;
@@ -23,5 +24,39 @@ class Instructor extends Person {
     }
     grade(student, subject) {
         return `${student.name} receives a perfect score in ${subject}.`;
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttrs) {
+        super(studentAttrs);
+        this.previousBackground = studentAttrs.previousBackground;
+        this.className = studentAttrs.className;
+        this.favSubjects = studentAttrs.favSubjects; // make array in instances
+    }     
+    listsSubjects() {
+        return ${this.favSubjects}.toString(" ");
+    }
+    PRAssignment(subject) {
+        return `${student.name} has submitted a PR for {subject}`;
+    }
+    sprintChallenge(subject) {
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }
+
+}
+
+class ProjectManager extends Instructor {
+    constructor(pmAttrs) {
+        super(pmAttrs);
+        this.gradClassName = pmAttrs.gradClassName;
+        this.favInstructor = pmAttrs.favInstructor;
+    }
+    standUp(channel) {
+    return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code in ${subject}`;
+    
     }
 }
