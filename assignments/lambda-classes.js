73a5afc219c2f8419b8e933d3544b21279196b10
unknown <38929577+meginks@users.noreply.github.com>
2019-01-24 14:58:08 -0600

    finished person and instructor classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,27 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+        this.gender = attributes.gender;
+    }
+    speak() {
+        return `Hello, my name is ${this.name}, and I am from ${this.location}`;
+    }
+};
+
+class Instructor extends Person {
+    constructor(instructorAttrs) {
+        this.specialty = instructorAttrs.specialty;
+        this.favLanguage = instructorAttrs.favLanguage;
+        this.catchPhrase = instructorAttrs.catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}.`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score in ${subject}.`;
+    }
+}
